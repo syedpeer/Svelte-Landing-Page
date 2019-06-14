6163086eec2cbb11085157b3a2b9da21c5c70216
Josse3 <josse.cottenier@hotmail.com>
2019-06-14 18:55:18 +0200

    Search bar

--- src/MainText.svelte
+++ src/MainText.svelte
@@ -28,15 +28,61 @@
   setInterval(() => {
     date = new Date();
   }, 1000);
+
+  let searchTerms = "";
+
+  const search = () => {
+    const encodedSearchTerms = searchTerms.replace(" ", "%20");
+    window.open(`https://ecosia.org/search?q=${encodedSearchTerms}`);
+    searchTerms = "";
+  };
 </script>
 
 <style>
   h1.timer {
     font-size: 6em;
   }
+
+  .searchbar-container {
+    margin-top: 2rem;
+    width: 45em;
+    border: none;
+    border-radius: 1rem;
+    overflow: hidden;
+    background-color: white;
+  }
+
+  .searchbar {
+    width: 80%;
+    padding: 1em;
+    border: none;
+  }
+
+  .search-button {
+    width: 18%;
+    height: 2.2rem;
+    background-color: rgb(7, 7, 168);
+    color: white;
+    border: none;
+    border-radius: 1rem;
+    cursor: pointer;
+  }
 </style>
 
 <div class="main-text" style="color: {color}">
   <h1 class="greeting">Good {timeOfDay}, Josse!</h1>
   <h1 class="timer">{`${hour}:${minutes}:${seconds}`}</h1>
+  <div class="searchbar-container">
+    <input
+      type="text"
+      bind:value={searchTerms}
+      class="searchbar"
+      placeholder="Search on Ecosia..."
+      on:keypress={event => {
+        if (event.key === 'Enter') search();
+      }} />
+    <button class="search-button" on:click={search}>
+      <i class="fa fa-search" />
+    </button>
+  </div>
 </div>
